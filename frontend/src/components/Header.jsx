99745a054a5d01bbca2c036f7991c7dab90a35e1
Minh Hoang <hoangtran234176@gmail.com>
2025-12-23 22:23:37 +0700

    feat(components): create file Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Trang chủ</li>
+                    <li>Đăng nhập</li>
+                    <li>Đăng ký</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes.jsx'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
+            {/* Header */}
+            <Header/>
             {/* Routes */}
             <AppRoutes/>
         </BrowserRouter>
